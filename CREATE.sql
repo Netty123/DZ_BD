a9d59314fedb364a338529e8c58d59327d30ce06
Netty <grozazubastyjdrakon@gmail.com>
2026-02-22 18:08:11 +0300

    Сделать таблицы и заполнить БД данными

---
+++ CREATE.sql
@@ -0,0 +1,46 @@
+CREATE TABLE IF NOT EXISTS performers (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS genre (
+	id SERIAL PRIMARY KEY,
+	genre_name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS performers_genre (
+	id_performer INTEGER REFERENCES performers(id),
+	id_genre INTEGER REFERENCES genre(id),
+	CONSTRAINT pk_performer_genre PRIMARY KEY (id_performer, id_genre)
+);
+
+CREATE TABLE IF NOT EXISTS albums (
+	id SERIAL PRIMARY KEY,
+	name_albums VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS performers_albums (
+	id_performer INTEGER REFERENCES performers(id),
+	id_album INTEGER REFERENCES albums(id),
+	CONSTRAINT pk_performer_album PRIMARY KEY (id_performer, id_album)
+);
+
+CREATE TABLE IF NOT EXISTS tracks (
+	id SERIAL PRIMARY KEY,
+	track_name VARCHAR(50) NOT NULL,
+	album INTEGER REFERENCES albums(id),
+	duration INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS collections (
+	id SERIAL PRIMARY KEY,
+	collection_name VARCHAR(50) NOT NULL,
+	year_of_release INTEGER
+);
+
+
+CREATE TABLE IF NOT EXISTS collections_track (
+	id_collection INTEGER REFERENCES collections(id),
+	id_track INTEGER REFERENCES tracks(id),
+	CONSTRAINT pk_collection_track PRIMARY KEY (id_collection, id_track)
+);
---
+++ INSERT.sql
@@ -0,0 +1,51 @@
+INSERT INTO performers(name)
+VALUES
+('Morgenshtern'),
+('KENTUKU'),
+('Пошлая Молли'),
+('Папин Олимпос'),
+('ooes');
+
+INSERT INTO genre(genre_name)
+VALUES
+('Поп'),
+('Рок'),
+('Классика'),
+('Рэп'),
+('Фанк');
+
+INSERT INTO albums(name_albums)
+VALUES
+('До того как стал известен'),
+('Last One'),
+('Снова твой цвет'),
+('В тишине тоскую'),
+('Олимпос'),
+('Медляк');
+
+INSERT INTO tracks(track_name, album, duration)
+VALUES
+('Замигает свет', 3, 179),
+('Быстро', 2, 137),
+('Домой', 2, 148),
+('Динозаврики', 5, 189),
+('Огонь', 5, 163),
+('Алёша', 5, 168);
+
+INSERT INTO collections(collection_name, year_of_release)
+VALUES
+('Хиты 2020', 2020),
+('Хиты 2021', 2021),
+('Хиты 2022', 2022),
+('Хиты 2023', 2023),
+('Хиты 2024', 2024),
+('Хиты 2025', 2025);
+
+INSERT INTO performers_albums(id_performer, id_album)
+VALUES (1, 1), (1, 2), (2, 3), (2, 4), (4, 5), (4,6);
+
+INSERT INTO performers_genre(id_performers, id_genre)
+VALUES (1, 2), (1, 4), (2, 1), (2, 2), (3, 1), (3, 2), (4, 1), (4, 2), (5, 1), (5, 2);
+
+INSERT INTO collections_track(id_collection, id_track)
+VALUES (2, 2), (2, 3), (2, 4), (3, 5), (4, 5), (6, 1)
