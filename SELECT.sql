20bd502c0c5b5b8cb4e5b2115d14d687ba3335db
Netty <grozazubastyjdrakon@gmail.com>
2026-02-23 14:28:11 +0300

    добавить третье задание

--- SELECT.sql
+++ SELECT.sql
@@ -20,3 +20,36 @@ WHERE name NOT LIKE '% %'
 -- 5 --
 SELECT track_name FROM tracks
 WHERE track_name LIKE '%Мой%'
+
+-- Задание №3 --
+
+-- 1 --
+SELECT genre.genre_name, COUNT(performers_genre.id_performers) FROM genre
+LEFT JOIN performers_genre ON genre.id = performers_genre.id_genre
+GROUP BY genre.id, genre.genre_name;
+
+-- 2 --
+SELECT COUNT(tracks.id) FROM tracks
+JOIN albums ON tracks.album = albums.id
+WHERE albums.release_date BETWEEN 2019 AND 2020;
+
+-- 3 --
+SELECT albums.name_albums, AVG(tracks.duration) FROM albums
+LEFT JOIN tracks ON albums.id = tracks.album
+GROUP BY albums.id, albums.name_albums
+ORDER BY AVG(tracks.duration) DESC;
+
+-- 4 --
+SELECT performers.name FROM performers
+WHERE performers.id NOT IN (SELECT DISTINCT performers_albums.id_performer FROM performers_albums
+JOIN albums ON performers_albums.id_album = albums.id
+WHERE albums.release_date = 2020
+);
+
+-- 5 --
+SELECT DISTINCT collections.collection_name FROM collections
+JOIN collections_track ON collections.id = collections_track.id_collection
+JOIN tracks ON collections_track.id_track = tracks.id
+JOIN performers_albums ON tracks.album = performers_albums.id_album
+JOIN performers ON performers_albums.id_performer = performers.id
+WHERE performers.name = 'Папин Олимпос';
